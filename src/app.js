dac03c88d83c430a262dcd6e1bbba64e8164c259
Lofthur <steintdale@gmail.com>
2017-09-04 14:02:33 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import style from './styles/app.sass';
+
+class App extends React.Component {
+	render (
+		return(
+			<h1>It's me again</h1>
+		);
+	);
+}
+
+render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlughin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+const cssDev = ['style-loader', 'css-loader', 'sass-loader'];
+const cssProd = ExtractTextPlugin.extract({
+	fallback: 'style-loader',
+	use: ['css-loader', 'style-loader']
+});
+
+const cssConfig = isProd ? cssProd : cssDev;
+
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		filename: 'app.bundle.js',
+		path: path.join(__dirname, './dist')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.sass$/,
+				use: cssConfig
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlughin({
+			title: 'Recipe Box',
+			template: '.src/index.html'
+		}),
+		new ExtractTextPlugin({
+			filename: 'css/style.css',
+			disable: !isProd,
+			allChunks: true
+		})
+	]
+}
+
