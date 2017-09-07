bce59b1f157e255481b4f6baabc1ce275dd08166
Lofthur <steintdale@gmail.com>
2017-09-07 12:28:00 +0200

    Setup form component

--- src/components/AddRecipeForm.js
+++ src/components/AddRecipeForm.js
@@ -3,9 +3,32 @@ import React from 'react';
 export class AddRecipeForm extends React.Component {
 	render() {
 		return(
-			<div>
-				<h1>From RecipeForm</h1>
-			</div>
+			<form action="#">
+				<h1>Add Recipe</h1>
+				<div>
+					<label htmlFor="recipe-name">Recipe Name: </label>
+					<input type="text" id="recipe-name" name="recipe_name" />
+				</div>
+				<div>
+					<label htmlFor="ingredient-name">Ingredient: </label>
+					<input type="text" id="ingredient-name" name="ingredient_name" />
+					<button>Add</button>
+				</div>
+				<div>
+					<ul>
+						<li>Banana</li>
+						<li>Salt</li>
+						<li>Pepper</li>
+					</ul>
+				</div>
+				<div>
+					<label htmlFor="description">Description</label>
+					<textarea name="description" id="" cols="30" rows="10"></textarea>
+				</div>
+				<div>
+					<button type="submit">Add Recipe</button>
+				</div>
+			</form>
 		);
 	}
 }
