189677250f450a2f4e53fe49d69c969cd7dce880
Lofthur <steintdale@gmail.com>
2017-09-21 13:11:59 +0200

    Can add in EditRecipeForm

--- src/components/EditRecipeForm.js
+++ src/components/EditRecipeForm.js
@@ -8,7 +8,33 @@ export class EditRecipeForm extends React.Component {
 		this.saveEdit = this.saveEdit.bind(this);
 		this.removeIngredient = this.removeIngredient.bind(this);
 		this.changeHandeler = this.changeHandeler.bind(this);
+		this.addIngredient = this.addIngredient.bind(this);
+		this.updateIngrString = this.updateIngrString.bind(this);
 
+		this.state = {
+			ingredientString: ''
+		}
+	}
+
+	updateIngrString(e) {
+		this.setState({
+			ingredientString: e.target.value
+		});
+
+		console.log(`e: ${e.target.value}`);
+		console.log(this.state.ingredientString);
+	}
+
+	addIngredient(e, value) {
+		const recipe = this.props.recipes[this.props.editKey];
+		const ingredients = recipe.ingr;
+		ingredients.push(value);
+		const updateRecipe = {
+			...recipe,
+			[e.target.name]: ingredients
+		}
+
+		this.props.updateRecipe(this.props.editKey, updateRecipe);
 	}
 
 	removeIngredient(e, value) {
@@ -45,6 +71,8 @@ export class EditRecipeForm extends React.Component {
 				<h3>Edit Recipe</h3>
 				<form onSubmit={this.saveEdit} data-key="edit">
 					<input type="text" name="name" value={recipe.name} onChange={this.changeHandeler} />
+					<input type="text" placeholder="Ingredient" value={this.state.ingredientString} onChange={this.updateIngrString} />
+					<button type="button" name="ingr"onClick={(e) => this.addIngredient(e, this.state.ingredientString)}>Add</button>
 					<ul>
 						{recipe.ingr.map((key, i) => {
 							return (
@@ -57,6 +85,7 @@ export class EditRecipeForm extends React.Component {
 					</ul>
 					<textarea name="desc" cols="30" rows="10" value={recipe.desc} onChange={this.changeHandeler}></textarea>
 					<button type="submit">Save</button>
+					<button type="button">Cancel</button>
 				</form>
 			</div>
 		)
